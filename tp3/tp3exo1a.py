8bd23f113267b3ea0206c6045c849af84ddb3bd8
Al3m1rr <atakanemirgcr@gmail.com>
2025-11-18 16:13:48 +0100

    TP2

---
+++ tp3/tp3exo1a.py
@@ -0,0 +1,7 @@
+N = int(input("Saisir un nombre : "))
+S = 0
+
+for i in range(0,N+1) :
+    S+= i
+
+print(S)
---
+++ tp3/tp3exo1b.py
@@ -0,0 +1,5 @@
+while True:
+    valeur_saisi = int(input("Saisir un nombre : "))
+
+    if valeur_saisi == 100 :
+        break
